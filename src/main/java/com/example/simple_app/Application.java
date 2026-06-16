d107c62485c1c8589de3b55da60cf84525d47d8a
Alifah Ilyana <2024420274@student.uitm.edu.my>
2026-06-16 22:26:09 +0800

    Add Insync frontend to static folder

--- src/main/java/com/example/simple_app/Application.java
+++ src/main/java/com/example/simple_app/Application.java
@@ -1,19 +1,11 @@
-package com.example.simpleapp;
+package com.example.simple_app;
 
-   import org.springframework.boot.SpringApplication;
-   import org.springframework.boot.autoconfigure.SpringBootApplication;
-   import org.springframework.web.bind.annotation.GetMapping;
-   import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-   @SpringBootApplication
-   @RestController
-   public class Application {
-       public static void main(String[] args) {
-           SpringApplication.run(Application.class, args);
-       }
-
-       @GetMapping("/")
-       public String home() {
-           return "<h1>Welcome to my Heroku-deployed Java Application!</h1>";
-       }
-   }
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
